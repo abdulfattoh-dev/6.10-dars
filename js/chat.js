acc9f46c3fe18c40d768098cb9466efce0609c68
abdulfattoh-dev <abdulfattoh.dev@gmail.com>
2025-07-20 10:23:07 +0500

    errors ficed

--- js/chat.js
+++ js/chat.js
@@ -4,21 +4,42 @@ const contentEl = document.querySelector(".content");
 const formEl = document.querySelector(".form");
 const inputEl = document.querySelector(".input");
 
+const getTimeFormat = () => {
+    let date = new Date();
+    return `${date.getHours()}:${date.getMinutes()}`;
+};
+
+const randomMessage = () => {
+    let index = Math.floor(Math.random() * DATA.length);
+    const divEl = document.createElement("div");
+    divEl.className = "msg";
+
+    divEl.innerHTML = `
+    <p>${DATA[index]}</p>
+    <span>${getTimeFormat()}</span>
+  `;
+
+    contentEl.appendChild(divEl);
+};
+
 formEl.addEventListener("submit", (event) => {
     event.preventDefault()
 
     const value = inputEl.value;
 
-    if (value.trim = !0) {
-        const divEl = document.createElement("div");
-        divEl.className = "msg me";
+    if (!value.trim()) return null;
 
-        divEl.innerHTML = `
-        <p>${value}</p>
-        <span>9:54</span>
-    `
+    const divEl = document.createElement("div");
+    divEl.className = "msg me"
+    divEl.innerHTML = `
+  <p>${value}</p>
+  <span>${getTimeFormat()}</span>
+  `;
 
-        contentEl.appendChild(divEl);
-        inputEl.value = ""
-    }
+    contentEl.appendChild(divEl)
+    inputEl.value = "";
+    setTimeout(() => {
+        randomMessage();
+
+    }, 2000)
 })
